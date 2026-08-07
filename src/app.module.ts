35839a199c2e96c76ac255dd6c7e7db2cfcdbf64
Felipe Assis <aoptop1@hotmail.com>
2026-08-06 21:05:46 -0300

    chore: ajustando app.module com a dependencia do loggermodule e criacao de rota health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    LoggerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        pinoHttp: {
+          level: config.get<string>('LOG_LEVEL') ?? 'info',
+          redact: {
+            paths: ['req.headers.authorization'],
+            censor: '[REDACTED]',
+          },
+          transport:
+            config.get<string>('NODE_ENV') !== 'production'
+              ? { target: 'pino-pretty' }
+              : undefined,
+        },
+        exclude: [{ method: RequestMethod.GET, path: 'health' }],
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
